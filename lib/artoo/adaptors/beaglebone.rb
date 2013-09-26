c15916d68ff940086ca61c9f63759b58139cde93
Adrian Zankich <adrian@hybridgroup.com>
2013-09-25 18:55:52 -0700

    Fix typo

--- lib/artoo/adaptors/beaglebone.rb
+++ lib/artoo/adaptors/beaglebone.rb
@@ -41,7 +41,7 @@ module Artoo
         File.open(value_file pin, 'w') {|f| f.write(val == :high ? "1" : "0") } if File.open(direction_file pin, 'r').read.to_sym == :out
       end
 
-      def digital_read pin
+      def digital_read controller
         pin = translate_pin controller
         (File.open(value_file pin, 'r').read == :high ? "1" : "0") if File.open(direction_file pin, 'r').read.to_sym == :out
       end
